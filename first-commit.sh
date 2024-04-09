681ba4d2d625ee69300a3585f8f1e7cfaf5f5c59
José Oliveira <jose.oliveira@sso.dev.br>
2024-04-08 22:14:12 -0300

    First edition GitIgnore

--- first-commit.sh
+++
@@ -1,7 +0,0 @@
-echo "## DNS server in Docker Swarm!!!" >> README.md
-git init
-git add README.md
-git commit -m "First DNS server commit to Docker Swarm!!!!!!"
-git branch -M main
-git remote add origin git@github.com:zecaoliveira/dns-in-docker-swarm.git
-git push -u origin main
